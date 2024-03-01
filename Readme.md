051474d459153e9ee3c7bf618ef55db6d6fa31df
Andrya0 <146868362+Andrya0@users.noreply.github.com>
2024-03-01 20:38:28 +0300

    Update to PyQT5
    Update

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Промежуточный проект
+
+pip freeze > requirements.txt для создания файла с библиотеками
+
+pip install -r requirements.txt для установки библиотек с файла
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import pygame, requests, sys, os
-from PyQt5 import Qt
+import os.path
+import sys
+
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from pip._vendor import requests
 
 
 class MapParams(object):
@@ -11,15 +17,48 @@ class MapParams(object):
 
     def ll(self):
         return str(self.lon) + "," + str(self.lat)
+
     def update(self, event):
         if event.key() == Qt.Page_UP and self.zoom < 19:
             self.zoom += 1
         elif event.key() == Qt.Key_Page_DOWN and self.zoom > 2:
             self.zoom -= 1
 
+
+class WindowMain(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('window.ui', self)  # Загружаем дизайн
+        self.initUI()
+        self.pushButton.clicked.connect(self.search)
+
+    def initUI(self):
+        self.setWindowTitle('Отображение картинки')
+        load_map(MapParams())
+        if os.path.exists('map.png'):
+            ## Изображение
+            self.pixmap = QPixmap('map.png')
+            # Если картинки нет, то QPixmap будет пустым,
+            # а исключения не будет
+            self.image = QLabel(self)
+            self.image.move(80, 60)
+            self.image.resize(250, 250)
+            # Отображаем содержимое QPixmap в объекте QLabel
+            self.image.setPixmap(self.pixmap)
+
+    def search(self):
+        print('я работаю!')
+
+
 def load_map(mp):
-    map_request = "http://static-maps.yandex.ru/1.x/?ll={ll}&z={z}&l={type}".format(ll=mp.ll(), z=mp.zoom, type=mp.type)
-    response = requests.get(map_request)
+    map_request = "http://static-maps.yandex.ru/1.x/"
+    params = {
+        "ll": mp.ll(),
+        "spn": ",".join([str(mp.zoom), str(mp.zoom)]),
+        "l": mp.type
+    }
+    response = requests.get(map_request, params=params)
+    print(response)
     if not response:
         print("Ошибка выполнения запроса:")
         print(map_request)
@@ -37,21 +76,10 @@ def load_map(mp):
 
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode((600, 450))
-    mp = MapParams()
-    while True:
-        event = pygame.event.wait()
-        if event.type == pygame.QUIT:
-            break
-        elif event.type == pygame.KEYUP:
-            mp.update(event)
-        map_file = load_map(mp)
-        screen.blit(pygame.image.load(map_file), (0, 0))
-        pygame.display.flip()
-    pygame.quit()
-    os.remove(map_file)
-
+    app = QApplication(sys.argv)
+    ex = WindowMain()
+    ex.show()
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
     main()
